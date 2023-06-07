611bda122f531bec7fcebe9caa85514629baa04c
Felipe <fabwebbr@gmail.com>
2023-06-07 12:07:08 -0300

    Atualização na geração de backups

--- GerarBackup.sh
+++ GerarBackup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Qual revendedor será migrado?
+REVENDA="xyz"
+
 # Onde os backups ficarão salvos?
-TOGENERATE="/backup_hdd/backups"
+TOGENERATE="/home/admin/admin_backups/"
 
 # Servidor de destino
 HOST="da2.srvhr.com.br"
@@ -12,38 +15,34 @@ PORTA="22022"
 #####################################################################################
 ######################## Abaixo já está tudo pronto #################################
 
-# Migrando os revendedores antes de tudo....
-for a in `cat /usr/local/directadmin/data/admin/reseller.list`; do
- echo "Gerando backup do Revendedor: $a";
- /usr/local/directadmin/directadmin admin-backup --destination=${TOGENERATE} --user=$a;
+# Migrando a conta principal do revendedor....
+ echo "Gerando backup do Revendedor: $REVENDA";
+ /usr/local/directadmin/directadmin admin-backup --destination=${TOGENERATE} --user=$REVENDA;
 
  echo "Enviando para destino...";
- rsync -avzhe 'ssh -p ${PORTA} -i ${CHAVESSH}' ${TOGENERATE}/reseller.admin.$a.tar.gz ${USUARIO}@${HOST}:/home/admin/admin_backups/;
- ssh -p ${PORTA} -i ${CHAVESSH} ${USUARIO}@${HOST} 'chown -R admin:admin /home/admin/admin_backups';
+ rsync -avzhe "ssh -p ${PORTA} -i ${CHAVESSH}" ${TOGENERATE}/reseller.admin.$REVENDA.tar.gz ${USUARIO}@${HOST}:/home/admin/admin_backups/;
+ ssh -p ${PORTA} -i ${CHAVESSH} ${USUARIO}@${HOST} "chown -R admin:admin /home/admin/admin_backups";
 
- echo "Arquivo enviado e adicionado à fila de restauração."
- ssh -p ${PORTA} -i ${CHAVESSH} ${USUARIO}@${HOST} "/bin/sh /root/Restore.sh admin ${a}";
+ echo "Arquivo enviado para o destino e já pode ser restaurado"
 
  echo "---------------------------------------------------"
- rm -rf ${TOGENERATE}/reseller.admin.$a.tar.gz;
+ rm -rf ${TOGENERATE}/reseller.admin.$REVENDA.tar.gz;
  sleep 10;
-done;
 
-echo "Pausa para o café e o servidor de destino respirar... (300s)"; sleep 300; echo "Café tomado.. vamos retomar.."
+echo "Pausa para o café e o servidor de destino respirar... (60s)"; sleep 60; echo "Café tomado.. vamos retomar.."
 
 # Migrando os clientes das revendas...
-for i in `cat /usr/local/directadmin/data/admin/reseller.list`; do
  echo "------------------------------------------";
- echo "Gerando backup dos clientes da Revenda: $i";
+ echo "Gerando backup dos clientes da Revenda: $REVENDA";
 
 # Inicia o backup dos clientes da revenda
- for b in `cat /usr/local/directadmin/data/users/$i/users.list`; do
+ for b in `cat /usr/local/directadmin/data/users/$REVENDA/users.list`; do
   echo "Gerando backup de $b";
   /usr/local/directadmin/directadmin admin-backup --destination=${TOGENERATE} --user=${b};
   sleep 5;
 
   echo "Enviando para destino...";
-  rsync -avzhe 'ssh -p ${PORTA} -i ${CHAVESSH}' ${TOGENERATE}/user.$i.$b.tar.gz ${USUARIO}@${HOST}:/home/admin/admin_backups/;
+  rsync -avzhe "ssh -p ${PORTA} -i ${CHAVESSH}" ${TOGENERATE}/user.$i.$b.tar.gz ${USUARIO}@${HOST}:/home/admin/admin_backups/;
   ssh -p ${PORTA} -i ${CHAVESSH} ${USUARIO}@${HOST} 'chown -R admin:admin /home/admin/admin_backups';
 
   ssh -p ${PORTA} -i ${CHAVESSH} ${USUARIO}@${HOST} "/bin/sh Restore.sh ${i} ${b}";
@@ -54,7 +53,5 @@ for i in `cat /usr/local/directadmin/data/admin/reseller.list`; do
  done;
 # Fim do backup dos clientes da revenda
 
- echo "Arquivos da revenda $i foram movidos. Nova pausa para o café (300s)...";
+ echo "Arquivos da revenda $REVENDA foram movidos.";
  echo "------------------------------------------";
- sleep 300;
-done
--- Restore.sh
+++ Restore.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-echo "action=restore&ip%5Fchoice=select&ip=50.114.62.104&local%5Fpath=%2Fhome%2Fadmin%2Fadmin%5Fbackups&owner=admin&select%30=user%2E${1}%2E${2}%2Etar%2Egz&type=admin&value=multiple&when=now&where=local" >> /usr/local/directadmin/data/task.queue
+IP="50.114.62.104"
+echo "action=restore&ip%5Fchoice=select&ip=${IP}&local%5Fpath=%2Fhome%2Fadmin%2Fadmin%5Fbackups&owner=admin&select%30=${1}%2E${2}%2E${3}%2Etar%2Egz&type=admin&value=multiple&when=now&where=local" >> /usr/local/directadmin/data/task.queue
